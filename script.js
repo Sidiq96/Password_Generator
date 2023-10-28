0015c1a05075d7d5b8f2a87fb7c95f2964b104b2
Sidiq Hersi <hersisidiq@gmail.com>
2023-10-28 20:12:45 +0100

    adding code to the js file which prompts the user to choose the length of password that they want

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
+  '@', '%', '+', '\\', '/', "'", '!', '#','$','^', '?', ':', ',',')',
   '(',
   '}',
   '{',
@@ -90,7 +77,38 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Prompt for password length
+  var passwordLength = prompt('How long do you want your password to be? (It must be between 8 and 128 characters)');
+  var length = parseInt(passwordLength);
 
+  // this code ensures that the user's input for the password length is a valid number within the specified range and provides feedback to the user if the input is not valid.
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert('Invalid input. Please enter a number between 8 and 128 for the password length.');
+    return;
+  }
+
+  // Ask user for character types to include
+  var includeSpecial = confirm('Include special characters?');
+  var includeNumeric = confirm('Include numeric characters?');
+  var includeLowercase = confirm('Include lowercase characters?');
+  var includeUppercase = confirm('Include uppercase characters?');
+
+  if (!includeSpecial && !includeNumeric && !includeLowercase && !includeUppercase) {
+    // This if statement checks to see if the user has selected at least one of these or otherwise they have to re-click the button
+    alert('At least one character type must be selected.');
+    return;
+  }
+
+  // Create an object to store user choices
+  var passwordOptions = {
+    length: length,
+    includeSpecial: includeSpecial,
+    includeNumeric: includeNumeric,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+  };
+
+  return passwordOptions;
 }
 
 // Function for getting a random element from an array
@@ -100,7 +118,7 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  var options = getPasswordOptions();
 }
 
 // Get references to the #generate element
@@ -110,7 +128,7 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
+  
   passwordText.value = password;
 }
 
